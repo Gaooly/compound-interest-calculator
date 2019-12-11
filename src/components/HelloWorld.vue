573ac54c3c046c8f392c1a6effa28ab7d05074d2
UhUh <489671546@qq.com>
2019-12-11 15:31:13 +0800

    1.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,87 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <el-row :gutter="20" type="flex" class="row-bg" justify="center">
+      <el-col :span="4">
+        <el-input placeholder="本金" v-model="input1">
+          <template slot="append">本金</template>
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input placeholder="复投" v-model="input2">
+          <template slot="append">复投</template>
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input placeholder="利率" v-model="input3">
+          <template slot="append">%</template>
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-input placeholder="" v-model="input4">
+          <template slot="append">复利次数</template>
+        </el-input>
+      </el-col>
+      <el-col :span="3">
+        <el-button type="primary" @click="calculation">计算</el-button>
+      </el-col>
+    </el-row>
+
+    <div class="table">
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column prop="time" label="次数"></el-table-column>
+        <el-table-column prop="all" label="总资产"></el-table-column>
+        <el-table-column prop="originally" label="本金"></el-table-column>
+        <el-table-column prop="profit" label="利润"></el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      input1: 0,
+      input2: 0,
+      input3: 10,
+      input4: 12,
+      tableData: []
+    };
+  },
+  methods: {
+    calculation() {
+      var tableData = [];
+      // all 最终收入
+      // originally 本金
+      // r 年利率
+      // n 存了多少年
+      // A 复投
+      var originally = parseFloat(this.input1)+0;
+      var A = parseFloat(this.input2)+0;
+      var r = parseFloat(this.input3) / 100 + 1;
+      var all = originally;
+      var n = parseFloat(this.input4)+0;
+      for (var i = 0; i < n; i++) {
+        var time = i + 1 + "次";
+        all = parseFloat(all * r + A).toFixed(2);
+        originally += A;
+        var profit = parseFloat(all - originally).toFixed(2);
+        tableData.push({
+          time,
+          all,
+          originally,
+          profit
+        });
+      }
+      this.tableData = tableData;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +99,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.table {
+  padding: 50px 200px;
 }
 </style>
